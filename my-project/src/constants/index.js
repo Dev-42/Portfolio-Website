b9a593a52cb61ecd2b4eb40bb33e9176d2c27bd3
server_42 <devbhattacharya42@gmail.com>
2024-08-04 17:43:52 +0530

    Project page done

--- my-project/src/constants/index.js
+++ my-project/src/constants/index.js
@@ -135,49 +135,41 @@ export const projects = [
   {
     iconUrl: pricewise,
     theme: "btn-back-red",
-    name: "Amazon Price Tracker",
+    name: "StockInsight Pro",
     description:
-      "Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.",
-    link: "https://github.com/adrianhajdin/pricewise",
+      "Developed an advanced AI-driven web application for fundamental stock analysis using ReactJS and the Gemini API, providing investors with precise valuations and actionable insights they can rely on for the next five years.The most trusted tool for the investors worldwide.",
+    link: "https://github.com/Dev-42/A.I-Stock-Prediction-Tool-",
   },
   {
     iconUrl: threads,
     theme: "btn-back-green",
-    name: "Full Stack Threads Clone",
+    name: "Mr.Chef | Culinary Express App",
     description:
-      'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-    link: "https://github.com/adrianhajdin/threads",
+      "Developed an end-to-end full-stack application revolutionizing food delivery with secure Firebase authentication, dynamic product display via Mr.Chef's API, optimized search with debouncing, and seamless Razorpay integration for secure payments.",
+    link: "https://github.com/Dev-42/Mr.Chef-Culinary-Express-App",
   },
   {
     iconUrl: car,
     theme: "btn-back-blue",
-    name: "Car Finding App",
+    name: "HealthKart 2.0",
     description:
-      "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
-    link: "https://github.com/adrianhajdin/project_next13_car_showcase",
+      "I led a team of 5 to build HealthKart 2.0, a one-stop destination for medicines, supplements, and fitness equipment.",
+    link: "https://github.com/Swagh001/HealthKart-Clone",
   },
   {
     iconUrl: snapgram,
     theme: "btn-back-pink",
-    name: "Full Stack Instagram Clone",
+    name: "Express 2.0",
     description:
-      "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
-    link: "https://github.com/adrianhajdin/social_media_app",
+      "This web app was built by a total of 5 people to serve daily fashion requirement of the youth at their doorstep.",
+    link: "https://github.com/muskanthapa2000/express.com_shopping_website_clone",
   },
   {
     iconUrl: estate,
     theme: "btn-back-black",
-    name: "Real-Estate Application",
+    name: "Personal Portfolio",
     description:
-      "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
-    link: "https://github.com/adrianhajdin/projects_realestate",
-  },
-  {
-    iconUrl: summiz,
-    theme: "btn-back-yellow",
-    name: "AI Summarizer Application",
-    description:
-      "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
-    link: "https://github.com/adrianhajdin/project_ai_summarizer",
+      "Developed a web application using react,react-router and threeJS to showcase my skills and experience.",
+    link: "https://github.com/Dev-42/Portfolio-Website",
   },
 ];
--- my-project/src/pages/Projects.jsx
+++ my-project/src/pages/Projects.jsx
@@ -1,8 +1,71 @@
+import { Link } from "react-router-dom";
+
+import CTA from "../components/CTA";
+import { projects } from "../constants/index";
+import { arrow } from "../assets/icons";
+
 const Projects = () => {
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <section className="max-container">
+      <h1 className="head-text">
+        My{" "}
+        <span className="blue-gradient_text drop-shadow font-semibold">
+          Projects
+        </span>
+      </h1>
+
+      <p className="text-slate-500 mt-2 leading-relaxed">
+        Throughout my journey as a web developer, I've embarked on numerous
+        projects, but these are the ones I hold closest to my heart. As a
+        passionate developer specializing in the MERN stack, I love building
+        projects and continuously seek to improve them. If you come across
+        something that piques your interest, feel free to explore the codebase
+        and share your thoughts or ideas. Your feedback is highly valued!
+      </p>
+
+      <div className="flex flex-wrap my-20 gap-16">
+        {projects.map((project) => (
+          <div className="lg:w-[400px] w-full" key={project.name}>
+            <div className="block-container w-12 h-12">
+              <div className={`btn-back rounded-xl ${project.theme}`} />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img
+                  src={project.iconUrl}
+                  alt="threads"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+
+            <div className="mt-5 flex flex-col">
+              <h4 className="text-2xl font-poppins font-semibold">
+                {project.name}
+              </h4>
+              <p className="mt-2 text-slate-500">{project.description}</p>
+              <div className="mt-5 flex items-center gap-2 font-poppins">
+                <Link
+                  to={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="font-semibold text-blue-600"
+                >
+                  Live Link
+                </Link>
+                <img
+                  src={arrow}
+                  alt="arrow"
+                  className="w-4 h-4 object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <hr className="border-slate-200" />
+
+      <CTA />
+    </section>
   );
 };
 
